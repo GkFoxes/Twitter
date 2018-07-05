3a46e948a981a2bb238d02fcf563439f56f37057
GkFoxes <dima25_matveenko@icloud.com>
2018-07-05 16:51:09 +0300

    Initial commit

---
+++ twitter-project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitter-project
+//
+//  Created by Дмитрий Матвеенко on 05.07.2018.
+//  Copyright © 2018 Дмитрий Матвеенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
