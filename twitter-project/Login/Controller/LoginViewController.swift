ad6fb87e4c2ae119620981d463a042d0cfe67924
GkFoxes <dima25_matveenko@icloud.com>
2018-07-20 20:26:16 +0300

    Bugfix with async (pre)

--- twitter-project/Login/Controller/LoginViewController.swift
+++ twitter-project/Login/Controller/LoginViewController.swift
@@ -57,35 +57,35 @@ class LoginViewController: UIViewController {
                 alertWarning(title: "Empty field", message: "You did not fill all the fields, please check again.")
                 return
         }
-        let queue = DispatchQueue.global(qos: .background)
-        queue.async{
-            DispatchQueue.main.async {
-                Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
-                    if error != nil {
-                        self.alertWarning(title: "Can not login", message: "Warning, please check the entered data.")
-                    }
-                    
-                    if user != nil {
-                        isLogin = true
-                        isLoginFirst = true
-                        
-                        self.performSegue(withIdentifier: "feedSegue", sender: nil)
-                    }
-                })
-                //print("Did download  image data")
-            }
-            //        Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
-            //            if error != nil {
-            //                self.alertWarning(title: "Can not login", message: "Warning, please check the entered data.")
-            //            }
-            //
-            //            if user != nil {
-            //                isLogin = true
-            //                isLoginFirst = true
-            //
-            //                self.performSegue(withIdentifier: "feedSegue", sender: nil)
-            //            }
+//        let queue = DispatchQueue.global(qos: .background)
+//        queue.async{
+//            DispatchQueue.main.async {
+//                Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+//                    if error != nil {
+//                        self.alertWarning(title: "Can not login", message: "Warning, please check the entered data.")
+//                    }
+//
+//                    if user != nil {
+//                        isLogin = true
+//                        isLoginFirst = true
+//
+//                        self.performSegue(withIdentifier: "feedSegue", sender: nil)
+//                    }
+//                })
+//                //print("Did download  image data")
+//            }
+                    Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            self.alertWarning(title: "Can not login", message: "Warning, please check the entered data.")
+                        }
+            
+                        if user != nil {
+                            isLogin = true
+                            isLoginFirst = true
+            
+                            self.performSegue(withIdentifier: "feedSegue", sender: nil)
+                        }
             self.alertWarning(title: "Can not login", message: "User does not exist, please check the entered data.")
-        }
+        })
     }
 }
