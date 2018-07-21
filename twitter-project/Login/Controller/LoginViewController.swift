8f55248e9becc9f079f7493c4041885eaf92810f
GkFoxes <dima25_matveenko@icloud.com>
2018-07-21 15:02:05 +0300

    Bugfix with Login Screen async

--- twitter-project/Login/Controller/LoginViewController.swift
+++ twitter-project/Login/Controller/LoginViewController.swift
@@ -57,35 +57,25 @@ class LoginViewController: UIViewController {
                 alertWarning(title: "Empty field", message: "You did not fill all the fields, please check again.")
                 return
         }
-//        let queue = DispatchQueue.global(qos: .background)
-//        queue.async{
-//            DispatchQueue.main.async {
-//                Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
-//                    if error != nil {
-//                        self.alertWarning(title: "Can not login", message: "Warning, please check the entered data.")
-//                    }
-//
-//                    if user != nil {
-//                        isLogin = true
-//                        isLoginFirst = true
-//
-//                        self.performSegue(withIdentifier: "feedSegue", sender: nil)
-//                    }
-//                })
-//                //print("Did download  image data")
-//            }
-                    Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
-                        if error != nil {
-                            self.alertWarning(title: "Can not login", message: "Warning, please check the entered data.")
-                        }
-            
-                        if user != nil {
-                            isLogin = true
-                            isLoginFirst = true
-            
-                            self.performSegue(withIdentifier: "feedSegue", sender: nil)
-                        }
-            self.alertWarning(title: "Can not login", message: "User does not exist, please check the entered data.")
-        })
+        
+        let queue = DispatchQueue.global(qos: .userInteractive)
+        queue.async{
+            Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error != nil {
+                    self.alertWarning(title: "Can not login", message: "Warning, please check the entered data.")
+                }
+                
+                if user != nil {
+                    isLogin = true
+                    isLoginFirst = true
+                    
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "feedSegue", sender: nil)
+                    }
+                } else {
+                    self.alertWarning(title: "Can not login", message: "User does not exist, please check the entered data.")
+                }
+            })
+        }
     }
 }
