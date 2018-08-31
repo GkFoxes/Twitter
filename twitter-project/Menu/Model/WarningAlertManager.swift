bcb7ecc3c0be1d39d06a6f4dffd0c6f3d20b1660
GkFoxes <dima25_matveenko@icloud.com>
2018-08-31 15:55:57 +0300

    Fix view

--- twitter-project/Menu/Model/WarningAlertManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  WarningAlertManager.swift
-//  twitter-project
-//
-//  Created by Дмитрий Матвеенко on 23.07.2018.
-//  Copyright © 2018 Дмитрий Матвеенко. All rights reserved.
-//
-
-import Foundation
