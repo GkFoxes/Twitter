336560f6fa830b6ccb644f97abd2ecd1c851a612
GkFoxes <dima25_matveenko@icloud.com>
2019-01-24 14:03:15 +0300

    Start rewriting project on Objective-C

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Дмитрий Матвеенко on 24/01/2019.
+//  Copyright © 2019 GkFoxes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
